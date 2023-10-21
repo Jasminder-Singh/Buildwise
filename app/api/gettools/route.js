e3ad7b9099924e37395713bd6e40307200f8dec8
Jasminder Singh <jassdhiman958@gmail.com>
2023-10-21 19:14:42 +0530

    delete disconnect

--- app/api/gettools/route.js
+++ app/api/gettools/route.js
@@ -10,7 +10,5 @@ export async function GET(){
         return NextResponse.json({message : "failed"},{status : 404});
     } catch(err){
         return NextResponse.json({message : "error"},{status : 500});
-    }finally{
-        await disconnectDB();
     }
 }
