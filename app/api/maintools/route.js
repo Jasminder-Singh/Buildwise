7d5d05e98344e2171cd284e0879ac70c16b28da0
Jasminder Singh <jassdhiman958@gmail.com>
2023-10-22 15:10:28 +0530

    route file

--- app/api/maintools/route.js
+++ app/api/maintools/route.js
@@ -1,17 +1,22 @@
-import dbConnect, { disconnectDB } from "@/db/dbcon";
-import toolModel from "@/db/schema/tool";
 import { NextResponse } from "next/server";
+import toolModel from "@/db/schema/tool";
+import dbConnect, { disconnectDB } from "@/db/dbcon";
 
-export async function GET(req){
-    try{
+export async function GET(req,res) {
+    try {
         await dbConnect();
-        console.log(await req.query);
+        const date = req.nextUrl.searchParams.get('date');
+        console.log(date);
         const tools = await toolModel.find();
-        if(tools) return NextResponse.json({tools},{status : 200});
-        return NextResponse.json({message : "failed"},{status : 404});
-    } catch(err){
-        return NextResponse.json({message : "error"},{status : 500});
-    } finally{
+
+        if (tools) return NextResponse.json({ tools }, { status: 200 });
+
+        return NextResponse.json({ message: "failed" }, { status: 404 });
+    } catch (err) {
+
+        return NextResponse.json({ message: "error" }, { status: 500 });
+
+    } finally {
         await disconnectDB();
     }
 }
